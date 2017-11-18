b1d25c981c7d2f009a228fe2ed176f31dbd9147a
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2017-11-18 11:22:29 -0500

    Created the create/delete poll routes

--- server/routes/poll.js
+++ server/routes/poll.js
@@ -4,8 +4,9 @@ const passport = require('passport');
 module.exports = function addPollRoutes(router) {
   indexRoute(router);
   singlePollRoute(router);
-  // deleteRoute(router);
-  // createRoute(router);
+  deleteRoute(router);
+  createRoute(router);
+  // updateRoute(router);
 }
 
 function indexRoute(router) {
@@ -31,11 +32,29 @@ function singlePollRoute(router) {
 function deleteRoute(router) {
   router.delete('/polls/:id', function deletePoll(req, res, next) {
     // TODO: authenticate with jwt and verify user matches poll owner
+    Poll.remove({_id: req.params.id}, function(err) {
+      if (err) return next(err);
+
+      res.status(200).json({message: "Poll deleted"});
+    })
   });
 }
 
 function createRoute(router) {
   router.post('/polls/create', function createPoll(req, res, next) {
     // TODO: authenticate with jwt
+    // TODO: validate poll data
+    const {title, options} = req.body;
+    const owner = req.user.username;
+    const poll = new Poll({
+      owner,
+      title,
+      options,
+    });
+
+    poll.save(function(err, poll) {
+      if (err) return next(err);
+      res.status(200).json({message: "Poll created successfully", poll});
+    });
   });
 }
