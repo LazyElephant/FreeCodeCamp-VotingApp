3087e38178dce4bf09807f2fa80842126b867d2b
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2017-11-17 20:34:56 -0500

    Created post routes for login and register

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,39 @@
+const passport = require('passport');
+const User = require('../models/user');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200)
-  res.json({message: "nothing to see here"})
+  res.json({success: true, message: "nothing to see here"})
+});
+
+router.post('/login', function login(req, res, next) {
+  // TODO: validate username/password
+  passport.authenticate('local', function(err, user, info) {
+    if (err) return next(err);
+    if (!user) return res.json({success: false, message: "No user with that name exists"});
+    
+    res.json({success: true, message:"Logged in"});
+  })(req, res, next);
+});
+
+router.post('/register', function register(req, res, next) {
+  // TODO: validate username/password
+  const user = new User();
+  user.username = req.body.username;
+  user.hashPassword(req.body.password);
+
+  user.save(function(err, user) {
+    if (err) return next(err);
+
+    res.json({
+      success: true, 
+      message:"User created successfully",
+    });
+
+  });
 });
 
 module.exports = router;
