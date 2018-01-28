f208fb22b160da81f0ad7e86ed2fefaf41914d67
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2018-01-28 15:30:29 -0500

    Style Login Page and layout with bootstrap

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,36 +4,66 @@ class Login extends React.Component<any, any> {
   private username: any;
   private password: any;
 
-  submit = (e: any) => {
+  constructor(props: any) {
+    super(props);
+
+    this.submit = this.submit.bind(this);
+  }
+
+  async submit(e: any) {
     e.preventDefault();
     // TODO: validate form data
     const username = this.username.value;
     const password = this.password.value;
 
-    fetch('/api/login', {
-      headers: [
-        ['Accept', 'application/json'],
-        ['Content-Type', 'application/json']
-      ],
-      credentials: 'same-origin',
-      method: 'POST',
-      body: JSON.stringify({username, password}),
-    })
-    .then(res => res.json())
-    .then(console.log);
+    try {
+      const res = await fetch('/api/login', {
+          headers: [
+            ['Accept', 'application/json'],
+            ['Content-Type', 'application/json']
+          ],
+          credentials: 'same-origin',
+          method: 'POST',
+          body: JSON.stringify({username, password}),
+        });
+      
+      if (res.status !== 200) {
+        // display error message
+      } 
+
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   render() {
     return (
-      <form onSubmit={this.submit} name="loginForm">
-        <label htmlFor="username">Username
-          <input type="text" name="username" ref={r => this.username = r}/>
-        </label>
-        <label htmlFor="password">Password
-          <input type="password" name="password" ref={r => this.password = r} />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
+      <div className="container mt-5">
+        <h2 className="mb-4">Log In</h2>
+        <form 
+          onSubmit={this.submit} 
+          name="loginForm">
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input 
+              className="form-control"
+              type="text"  
+              name="username" 
+              ref={r => this.username = r}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password form-label">Password</label>
+            <input 
+              className="form-control"
+              type="password"
+              name="password" 
+              ref={r => this.password = r} />
+          </div>
+          <button 
+            className="btn btn-primary"
+            type="submit">Log in</button>
+        </form>
+      </div>
     );
   }
 }
