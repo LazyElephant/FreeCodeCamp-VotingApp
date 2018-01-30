f55ffe31cc262f0361af145af8f179ef69d635cc
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2018-01-29 22:07:17 -0500

    Make protected routes store the intended path when a user attempts to visit while not logged in

--- client/src/components/ProtectedRouteHOC.tsx
+++ client/src/components/ProtectedRouteHOC.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { redirected } from '../actions'
 
 export default function makeProtectedRoute(C: any) {
-  class ProtectedRoute extends React.Component<{ isAuthenticated: any }, any> {
+  class ProtectedRoute extends React.Component<any, any> {
     render() {
-      if (!this.props.isAuthenticated) {
+      const { isAuthenticated, ...rest } = this.props
+      if (!isAuthenticated) {
+        const path = this.props.history.location.pathname
+        this.props.redirected(path)
         return <Redirect to="/signin" />
       }
 
-      return <C />
+      return <C {...rest}/>
     }
   }
 
   return connect(
-    (state: any) => ({isAuthenticated: state.user.isAuthenticated})
+    (state: any) => ({isAuthenticated: state.user.isAuthenticated}),
+    { redirected }
   )(ProtectedRoute)
 }
