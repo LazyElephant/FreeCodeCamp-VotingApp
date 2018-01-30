04c3f343ba55d72e5f8c18cc5dbf3661e59780f4
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2018-01-30 14:30:36 -0500

    Add action to clear redirect info after the redirect is completed

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -1,6 +1,7 @@
 export const LOGGED_IN = 'LOGGED_IN'
 export const LOGGED_OUT = 'LOGGED_OUT'
 export const REDIRECTED = 'REDIRECTED'
+export const CLEAR_REDIRECT = 'CLEAR_REDIRECT'
 
 export interface Action {
   type: string
@@ -20,11 +21,17 @@ export function logOut() {
   }
 }
 
-// might be better to move this to a routing specific file
-// but for now, it's staying since it's the only one
+// if more router functionality is added to redux, this will move
+// to a separate file
 export function redirected(path: string) {
   return {
     type: REDIRECTED,
     payload: path
+  }
+}
+
+export function clearRedirect() {
+  return {
+    type: CLEAR_REDIRECT
   }
 }
