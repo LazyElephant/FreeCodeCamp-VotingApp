9bf9ef4ea149d509d1a50a7ae2102f4e12cd57b1
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2018-01-29 22:06:28 -0500

    Create action/reducer to store the intended path when a user is redirected

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -1,9 +1,10 @@
 export const LOGGED_IN = 'LOGGED_IN'
 export const LOGGED_OUT = 'LOGGED_OUT'
+export const REDIRECTED = 'REDIRECTED'
 
 export interface Action {
   type: string
-  payload: any
+  payload?: any
 }
 
 export function logIn(email: string) {
@@ -16,5 +17,14 @@ export function logIn(email: string) {
 export function logOut() {
   return {
     type: LOGGED_OUT,
+  }
+}
+
+// might be better to move this to a routing specific file
+// but for now, it's staying since it's the only one
+export function redirected(path: string) {
+  return {
+    type: REDIRECTED,
+    payload: path
   }
 }
--- client/src/reducers/user.ts
+++ client/src/reducers/user.ts
@@ -1,18 +1,21 @@
-import { Action, LOGGED_IN, LOGGED_OUT } from '../actions'
+import { Action, LOGGED_IN, LOGGED_OUT, REDIRECTED } from '../actions'
 
 const initialState = {
   isAuthenticated: false,
-  email: ''
+  email: '',
+  redirectedFrom: '',
 }
 
 export default function reducer(state: any = initialState, action: Action) {
   switch (action.type) {
     case LOGGED_IN:
-      return { isAuthenticated: true, email: action.payload }
+      return { ...state, isAuthenticated: true, email: action.payload }
     
     case LOGGED_OUT:
-      return { isAuthenticated: false, email: '' }
+      return { ...state, isAuthenticated: false, email: '' }
 
+    case REDIRECTED:
+      return { ...state, redirectedFrom: action.payload}
     default:
       return state
   }
