15628dade776bdb98e5a8e00ec7e595d2b592d94
LazyElephant <tschmidt@lazyelephantstudio.com>
2018-03-02 19:53:21 -0500

    Add different navigation based on authentication status

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,31 +11,15 @@ class NavBar extends Component {
         <a className="navbar-brand col-auto" href="/">Pollster</a>
         {
           isAuthenticated ?
-            <SignOutButton name={username} onClick={logOut}/> :
-            <ToggleButton />             
+            <AuthenticatedNav name={username} onClick={logOut}/> :
+            <UnauthenticatedNav />             
         }
       </nav>
     )
   }
 }
 
-const SignOutButton = ({name, onClick}) => [
-  <span
-    className="navbar-text hidden-xs-down ml-auto mr-2"
-    key="nav-greeting"
-  >
-    Welcome, { name }
-  </span>,
-  <button 
-    className="btn btn-outline-secondary" 
-    key="logout-button"
-    onClick={onClick}
-  >
-    Sign Out
-  </button>
-]
-
-const ToggleButton = () => [
+const AuthenticatedNav = ({name, onClick}) => [
   <button 
     className="navbar-toggler" 
     type="button" 
@@ -54,11 +38,54 @@ const ToggleButton = () => [
     id="navbarSupportedContent"
     key="toggle-menu"
   >
-    <ul className="navbar-nav mr-auto text-right">
-      <li className="nav-item">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item ml-auto">
+        <a className="nav-link" href="/my-polls">My Polls</a>
+      </li>
+      <li className="nav-item ml-auto">
+        <a className="nav-link" href="/polls/create">Create</a>
+      </li>
+    </ul>
+    <span
+      className="navbar-text d-none d-lg-block mr-2"
+      key="nav-greeting"
+    >
+      Welcome, { name }
+    </span>
+    <button 
+      className="btn d-block btn-outline-secondary ml-auto ml-lg-0" 
+      key="logout-button"
+      onClick={onClick}
+    >
+      Sign Out
+    </button>
+  </div>,
+]
+
+const UnauthenticatedNav = () => [
+  <button 
+    className="navbar-toggler" 
+    type="button" 
+    data-toggle="collapse" 
+    data-target="#navbarSupportedContent" 
+    aria-controls="navbarSupportedContent" 
+    aria-expanded="false" 
+    aria-label="Toggle navigation"
+    key="toggle-button"
+  >
+    <span className="navbar-toggler-icon" />
+  </button>,
+
+  <div 
+    className="collapse navbar-collapse" 
+    id="navbarSupportedContent"
+    key="toggle-menu"
+  >
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item ml-auto">
         <a className="nav-link" href="/login">Sign In</a>
       </li>
-      <li className="nav-item">
+      <li className="nav-item ml-auto">
         <a className="nav-link" href="/signup">Sign Up</a>
       </li>
     </ul>
